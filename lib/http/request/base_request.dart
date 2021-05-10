c9f24e78b6de0d944bc9dc5b660bf6c71fb776e6
sheepshine <842293890@qq.com>
2021-05-10 22:04:46 +0800

    http request

---
+++ lib/http/request/base_request.dart
@@ -0,0 +1,41 @@
+enum HttpMethod { GET, POST, DELETE} 
+
+abstract class BaseRequset{
+  var pathParams;
+  var useHttps = true;
+  String authority() {
+    return "api.devio.orig";
+  }
+  HttpMethod httpMethod();
+  String path();
+  String url() {
+    Uri uri;
+    var pathStr = path();
+    if (pathParams!=null) {
+      if (path().endsWith("/")){
+        pathStr = "${path()}$pathParams";
+      } else {
+        pathStr = "${path()}/$pathParams";
+      }
+    }
+
+    if (useHttps) {
+      uri = Uri.https(authority(), pathStr, pathParams);
+    } else {
+      uri = Uri.http(authority(), pathStr, pathParams);
+    }
+    return uri.toString();
+  }
+  bool needLogin();
+  Map<String, String>params = Map();
+  BaseRequset add(String k, Object v) {
+    params[k] = v.toString();
+    return this;
+  }
+  Map<String, dynamic> header = Map();
+  BaseRequset addHeader(String k, Object v) {
+    params[k] = v.toString();
+    return this;
+  }
+
+}
